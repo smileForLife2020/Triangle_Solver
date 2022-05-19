5ffd1972a5168120415b9f0a7538be741551265d
smileForLife2020 <61994582+smileForLife2020@users.noreply.github.com>
2022-05-19 11:38:28 -0400

    Create sketch.js
    made a functional background that adjusts to the screen width and height (if the background isn't updated it means it worked perfectly)

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//stating the global variables
+var w = screen.width; //the screen width
+var h = screen.height; //the screen height
+
+function setup() {
+  createCanvas(w, h);
+}
+
+function draw() {
+  paper();
+}
+
+//making the background look like paper
+function paper () {
+  background(235);
+  strokeWeight(0.5);
+  
+  //drawing the lines
+  for(var i = 0; i <= 25; i++) {
+    if(i == 0) {
+      stroke(255, 92, 10);
+    }
+    else {
+      stroke(51, 175, 232);
+    }
+    line(0, h/14 + i* h/26.95, w, h/14 + i* h/26.95)
+  }
+}
